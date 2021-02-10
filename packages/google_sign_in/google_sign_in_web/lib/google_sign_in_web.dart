5485b5d0922ccab763c522ffe0214c7362821bc8
Hillel Coren <hillelcoren@gmail.com>
2021-02-10 13:07:46 +0200

    OAuth changes

--- packages/google_sign_in/google_sign_in_web/lib/google_sign_in_web.dart
+++ packages/google_sign_in/google_sign_in_web/lib/google_sign_in_web.dart
@@ -132,7 +132,6 @@ class GoogleSignInPlugin extends GoogleSignInPlatform {
   Future<GoogleSignInUserData> signIn() async {
     await initialized;
     try {
-      print('## signIn');
       final auth2.GoogleUser currentUser = await auth2.getAuthInstance().signIn();
       return gapiUserToPluginUserData(currentUser);
     } on auth2.GoogleAuthSignInError catch (reason) {
